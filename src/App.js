b232b1ef53cc3c130c48d6b0f270818134a30b02
emakiuchi <edwinyumakiuchi@gmail.com>
2023-06-30 18:12:00 -0700

    added highlighting functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import cheerio from 'cheerio';
 import fetch from 'isomorphic-fetch';
 
@@ -6,6 +6,8 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   const [matchedValues, setMatchedValues] = useState([]);
   const [players, setPlayers] = useState([]);
+  const [selectedValueIndex, setSelectedValueIndex] = useState(-1);
+  const inputRef = useRef(null);
 
   useEffect(() => {
     fetch('https://hashtagbasketball.com/fantasy-basketball-projections')
@@ -42,16 +44,55 @@ function App() {
       .map((player) => player.name)
       .filter((item) => item.toLowerCase().startsWith(value.toLowerCase()));
     setMatchedValues(filteredValues);
+    setSelectedValueIndex(-1); // Reset the selected value index
   };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      setSelectedValueIndex((prevIndex) => {
+        const newIndex = prevIndex > 0 ? prevIndex - 1 : matchedValues.length - 1;
+        return matchedValues[newIndex] ? newIndex : prevIndex;
+      });
+    } else if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      setSelectedValueIndex((prevIndex) => {
+        const newIndex = prevIndex < matchedValues.length - 1 ? prevIndex + 1 : 0;
+        return matchedValues[newIndex] ? newIndex : prevIndex;
+      });
+    } else if (e.key === 'Enter') {
+      e.preventDefault();
+      if (selectedValueIndex !== -1) {
+        const selectedValue = matchedValues[selectedValueIndex];
+        setInputValue(selectedValue);
+        setMatchedValues([]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   return (
     <div className="App">
+      <style>
+        {`
+        .selected {
+          background-color: yellow;
+        }
+      `}
+      </style>
       <header className="App-header">
         <input
           type="text"
           value={inputValue}
           onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
           placeholder="Enter text"
+          ref={inputRef}
         />
         <p>You entered: {inputValue}</p>
         {matchedValues.length > 0 && (
@@ -60,7 +101,11 @@ function App() {
             <span>
               {matchedValues.map((value, index) => (
                 <React.Fragment key={index}>
-                  {value}
+                  <span
+                    className={index === selectedValueIndex ? 'selected' : ''}
+                  >
+                    {value}
+                  </span>
                   {index !== matchedValues.length - 1 && <br />}
                 </React.Fragment>
               ))}
