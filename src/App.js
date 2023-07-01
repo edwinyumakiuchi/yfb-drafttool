dfc6e7198dfcca0beb575b5154bcdffa7c0b0163
emakiuchi <edwinyumakiuchi@gmail.com>
2023-06-30 17:59:12 -0700

    changed from test values to playernames

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import cheerio from 'cheerio';
+import fetch from 'isomorphic-fetch';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [matchedValues, setMatchedValues] = useState([]);
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://hashtagbasketball.com/fantasy-basketball-projections')
+      .then((response) => response.text())
+      .then((html) => {
+        const $ = cheerio.load(html);
+
+        const fetchedPlayers = [];
+
+        $('#ContentPlaceHolder1_GridView1 tr:has(td)').each((index, element) => {
+          const playerNameElement = $(element).find('a');
+          const playerName = playerNameElement.text().trim();
+
+          const lfgpElement = $(element).find('td:eq(6)');
+          const lfgpValue = lfgpElement.text().trim();
+
+          fetchedPlayers.push({
+            name: playerName,
+            lfgp: lfgpValue,
+            id: index,
+          });
+        });
+
+        setPlayers(fetchedPlayers);
+      });
+  }, []);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
 
     // Filter the matched values based on the input value
-    const filteredValues = ['abc', 'acd', 'bcd', 'cdf'].filter((item) =>
-      item.toLowerCase().startsWith(value.toLowerCase())
-    );
+    const filteredValues = players
+      .map((player) => player.name)
+      .filter((item) => item.toLowerCase().startsWith(value.toLowerCase()));
     setMatchedValues(filteredValues);
   };
 
