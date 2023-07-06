8a991aca64c0a669a856cc28db482004f27110ee
emakiuchi <edwinyumakiuchi@gmail.com>
2023-07-05 22:05:29 -0700

    cleaned code

--- storeData.js
+++ storeData.js
@@ -17,87 +17,75 @@ async function scrapeAndStoreData() {
     $('#ContentPlaceHolder1_GridView1 tr:has(td)').each((index, element) => {
       const playerNameElement = $(element).find('a');
       const playerName = playerNameElement.text().trim();
-      console.log(`playerName: ${playerName}`);
 
-        if (!playerName) {
-          return; // Skip to the next iteration if playerName is falsy
+      if (!playerName) {
+        return; // Skip to the next iteration if playerName is falsy
+      }
+
+      let playerRank = 0, playerADP = 0, playerPos = 0, playerTeam = 0, playerGP = 0, playerMPG = 0, playerFG = 0,
+        playerFGM = 0, playerFGA = 0, playerFT = 0, playerFTM = 0, playerFTA = 0, playerTPM = 0, playerPTS = 0,
+        playerTREB = 0, playerAST = 0, playerSTL = 0, playerBLK = 0, playerTO = 0, playerTotal = 0
+
+      $(element).find('td').each((index, tdElement) => {
+        const tdValue = $(tdElement).text().trim();
+
+        switch (index) {
+          case 0:
+            playerRank = tdValue
+            break;
+          case 1:
+            playerADP = tdValue
+            break;
+          case 3:
+            playerPos = tdValue
+            break;
+          case 4:
+            playerTeam = tdValue
+            break;
+          case 5:
+            playerGP = tdValue
+            break;
+          case 6:
+            playerMPG = tdValue
+            break;
+          case 7:
+            playerFG = tdValue
+            const matchFGResult = playerFG.match(/\((.*?)\)/);
+            [playerFGM, playerFGA] = matchFGResult[1].split('/').map(value => value.trim());
+            playerFG = playerFG.trim().split('\n')[0];
+            break;
+          case 8:
+            playerFT = tdValue
+            const matchFTResult = playerFT.match(/\((.*?)\)/);
+            [playerFTM, playerFTA] = matchFTResult[1].split('/').map(value => value.trim());
+            playerFT = playerFT.trim().split('\n')[0];
+            break;
+          case 9:
+            playerTPM = tdValue
+            break;
+          case 10:
+            playerPTS = tdValue
+            break;
+          case 11:
+            playerTREB = tdValue
+            break;
+          case 12:
+            playerAST = tdValue
+            break;
+          case 13:
+            playerSTL = tdValue
+            break;
+          case 14:
+            playerBLK = tdValue
+            break;
+          case 15:
+            playerTO = tdValue
+            break;
+          case 16:
+            playerTotal = tdValue
+            break;
         }
-
-      let playerRank = 0, playerADP = 0, playerPos = 0, playerTeam = 0, playerGP = 0, playerMPG = 0, playerFG = 0, playerFGM = 0, playerFGA = 0,
-        playerFT = 0, playerFTM = 0, playerFTA = 0, playerTPM = 0, playerPTS = 0, playerTREB = 0, playerAST = 0, playerSTL = 0, playerBLK = 0, playerTO = 0, playerTotal = 0
-
-        $(element).find('td').each((index, tdElement) => {
-          const tdValue = $(tdElement).text().trim();
-          // console.log(`index: ${index}, tdElement: ${tdElement}, tdValue: ${tdValue}`);
-
-          switch (index) {
-            case 0:
-              playerRank = tdValue
-              break;
-            case 1:
-              playerADP = tdValue
-              break;
-            case 3:
-              playerPos = tdValue
-              break;
-            case 4:
-              playerTeam = tdValue
-              break;
-            case 5:
-              playerGP = tdValue
-              break;
-            case 6:
-              playerMPG = tdValue
-              break;
-            case 7:
-              playerFG = tdValue
-              console.log(`playerFG: ${playerFG}`);
-              const matchFGResult = playerFG.match(/\((.*?)\)/);
-              [playerFGM, playerFGA] = matchFGResult[1].split('/').map(value => value.trim());
-              playerFG = playerFG.trim().split('\n')[0];
-              console.log(`playerFG: ${playerFG}`);
-              console.log(`playerFGM: ${playerFGM}`);
-              console.log(`playerFGA: ${playerFGA}`);
-              break;
-            case 8:
-              playerFT = tdValue
-              console.log(`playerFT: ${playerFT}`);
-              const matchFTResult = playerFT.match(/\((.*?)\)/);
-              [playerFTM, playerFTA] = matchFTResult[1].split('/').map(value => value.trim());
-              playerFT = playerFT.trim().split('\n')[0];
-              console.log(`playerFT: ${playerFT}`);
-              console.log(`playerFTM: ${playerFTM}`);
-              console.log(`playerFTA: ${playerFTA}`);
-              break;
-            case 9:
-              playerTPM = tdValue
-              break;
-            case 10:
-              playerPTS = tdValue
-              break;
-            case 11:
-              playerTREB = tdValue
-              break;
-            case 12:
-              playerAST = tdValue
-              break;
-            case 13:
-              playerSTL = tdValue
-              break;
-            case 14:
-              playerBLK = tdValue
-              break;
-            case 15:
-              playerTO = tdValue
-              break;
-            case 16:
-              playerTotal = tdValue
-              break;
-            default:
-              // Code to be executed when none of the cases match the expression
-              // console.log(`None of the cases match the switch expression!`);
-          }
-        });
+      });
 
       fetchedPlayers.push({ name: playerName,
         rank: playerRank,
