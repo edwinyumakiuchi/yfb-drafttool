5818e673d92ab27c1bea84cfee2715ee756602dc
emakiuchi <edwinyumakiuchi@gmail.com>
2023-07-09 16:28:36 -0700

    updated to implement api call to retrieve bearer token

--- src/App.js
+++ src/App.js
@@ -6,59 +6,89 @@ function App() {
   const [matchedValues, setMatchedValues] = useState([]);
   const [players, setPlayers] = useState([]);
   const [selectedValueIndex, setSelectedValueIndex] = useState(-1);
+  const [accessToken, setAccessToken] = useState('');
   const inputRef = useRef(null);
 
-useEffect(() => {
-  fetch('https://us-west-2.aws.data.mongodb-api.com/app/data-natmv/endpoint/data/v1/action/find', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer def',
-    },
-    body: JSON.stringify({
-      dataSource: 'Cluster0',
-      database: 'sample-nba',
-      collection: 'projections',
-      filter: {},
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('data:', data);
-      if (data.documents && Array.isArray(data.documents)) {
-        const fetchedPlayers = data.documents.map((player) => ({
-          _id: player._id,
-          name: player.name,
-          rank: player.rank,
-          adp: player.adp,
-          position: player.position,
-          team: player.team,
-          gp: player.gp,
-          minutesPerGame: player.minutesPerGame,
-          fieldGoal: player.fieldGoal,
-          fieldGoalMade: player.fieldGoalMade,
-          fieldGoalAttempt: player.fieldGoalAttempt,
-          freeThrow: player.freeThrow,
-          freeThrowMade: player.freeThrowMade,
-          freeThrowAttempt: player.freeThrowAttempt,
-          threePointMade: player.threePointMade,
-          points: player.points,
-          totalRebounds: player.totalRebounds,
-          assists: player.assists,
-          steals: player.steals,
-          blocks: player.blocks,
-          turnovers: player.turnovers,
-          total: player.total,
-        }));
-        setPlayers(fetchedPlayers);
-      } else {
-        console.error('Invalid data format:', data);
-      }
+  useEffect(() => {
+    // Make the login API call to get the access token
+    fetch('https://realm.mongodb.com/api/client/v2.0/app/data-natmv/auth/providers/local-userpass/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'edwinyumakiuchi@gmail.com',
+        password: 'abc',
+      }),
     })
-    .catch((error) => {
-      console.error('API call error:', error);
-    });
-}, []);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('login data:', data);
+        if (data.access_token) {
+          setAccessToken(data.access_token); // Store the access token
+        } else {
+          console.error('Access token not found in response:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Login API call error:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (accessToken) {
+      // Make the data fetch API call using the access token
+      fetch('https://us-west-2.aws.data.mongodb-api.com/app/data-natmv/endpoint/data/v1/action/find', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          dataSource: 'Cluster0',
+          database: 'sample-nba',
+          collection: 'projections',
+          filter: {},
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('data:', data);
+          if (data.documents && Array.isArray(data.documents)) {
+            const fetchedPlayers = data.documents.map((player) => ({
+              _id: player._id,
+              name: player.name,
+              rank: player.rank,
+              adp: player.adp,
+              position: player.position,
+              team: player.team,
+              gp: player.gp,
+              minutesPerGame: player.minutesPerGame,
+              fieldGoal: player.fieldGoal,
+              fieldGoalMade: player.fieldGoalMade,
+              fieldGoalAttempt: player.fieldGoalAttempt,
+              freeThrow: player.freeThrow,
+              freeThrowMade: player.freeThrowMade,
+              freeThrowAttempt: player.freeThrowAttempt,
+              threePointMade: player.threePointMade,
+              points: player.points,
+              totalRebounds: player.totalRebounds,
+              assists: player.assists,
+              steals: player.steals,
+              blocks: player.blocks,
+              turnovers: player.turnovers,
+              total: player.total,
+            }));
+            setPlayers(fetchedPlayers);
+          } else {
+            console.error('Invalid data format:', data);
+          }
+        })
+        .catch((error) => {
+          console.error('API call error:', error);
+        });
+    }
+  }, [accessToken]);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
