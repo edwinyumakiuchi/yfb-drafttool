32382b1c4a09212c56a539b4744cea3fdc37ac2e
emakiuchi <edwinyumakiuchi@gmail.com>
2023-07-11 12:58:58 -0700

    updated to have table fonts in bold

--- src/AppUI.js
+++ src/AppUI.js
@@ -16,29 +16,29 @@ function AppUI({
 
     return (
       <tr key={index} className={index === selectedValueIndex ? 'selected' : ''}>
-        <td className="centered">{player.rank}</td>
-        <td className="centered">{player.adp}</td>
-        <td className="centered">{player.name}</td>
-        <td className="centered">{player.position}</td>
-        <td className="centered">{player.team}</td>
-        <td className="centered">{player.gp}</td>
-        <td className="centered">{player.minutesPerGame}</td>
-        <td className="centered">{player.fieldGoal}</td>
-        <td className="centered">{player.fieldGoalMade}</td>
-        <td className="centered">{player.fieldGoalAttempt}</td>
-        <td className="centered">{player.freeThrow}</td>
-        <td className="centered">{player.freeThrowMade}</td>
-        <td className="centered">{player.freeThrowAttempt}</td>
-        <td className="centered">{player.threePointMade}</td>
-        <td className={isHighPoints ? 'centered green' : 'centered'}>
+        <td className="bold centered">{player.rank}</td>
+        <td className="bold centered">{player.adp}</td>
+        <td className="bold centered">{player.name}</td>
+        <td className="bold centered">{player.position}</td>
+        <td className="bold centered">{player.team}</td>
+        <td className="bold centered">{player.gp}</td>
+        <td className="bold centered">{player.minutesPerGame}</td>
+        <td className="bold centered">{player.fieldGoal}</td>
+        <td className="bold centered">{player.fieldGoalMade}</td>
+        <td className="bold centered">{player.fieldGoalAttempt}</td>
+        <td className="bold centered">{player.freeThrow}</td>
+        <td className="bold centered">{player.freeThrowMade}</td>
+        <td className="bold centered">{player.freeThrowAttempt}</td>
+        <td className="bold centered">{player.threePointMade}</td>
+        <td className={isHighPoints ? 'bold centered green' : 'bold centered'}>
           {player.points}
         </td>
-        <td className="centered">{player.totalRebounds}</td>
-        <td className="centered">{player.assists}</td>
-        <td className="centered">{player.steals}</td>
-        <td className="centered">{player.blocks}</td>
-        <td className="centered">{player.turnovers}</td>
-        <td className="centered">{player.total}</td>
+        <td className="bold centered">{player.totalRebounds}</td>
+        <td className="bold centered">{player.assists}</td>
+        <td className="bold centered">{player.steals}</td>
+        <td className="bold centered">{player.blocks}</td>
+        <td className="bold centered">{player.turnovers}</td>
+        <td className="bold centered">{player.total}</td>
       </tr>
     );
   });
@@ -49,31 +49,31 @@ function AppUI({
     <>
       {matchedValues.length > 0 && (
         <div>
-          <p>Matched values:</p>
+          <p></p>
           <table className="bordered-table">
-            <thead>
+            <thead className="header-row">
               <tr>
-                <th className="centered">RANK</th>
-                <th className="centered">ADP</th>
-                <th className="centered">PLAYER</th>
-                <th className="centered">POS</th>
-                <th className="centered">TEAM</th>
-                <th className="centered">GP</th>
-                <th className="centered">MPG</th>
-                <th className="centered">FG%</th>
-                <th className="centered">FGM</th>
-                <th className="centered">FGA</th>
-                <th className="centered">FT%</th>
-                <th className="centered">FTM</th>
-                <th className="centered">FTA</th>
-                <th className="centered">3PM</th>
-                <th className="centered">PTS</th>
-                <th className="centered">TREB</th>
-                <th className="centered">AST</th>
-                <th className="centered">STL</th>
-                <th className="centered">BLK</th>
-                <th className="centered">TO</th>
-                <th className="centered">TOTAL</th>
+                <th className="bold centered">RANK</th>
+                <th className="bold centered">ADP</th>
+                <th className="bold centered">PLAYER</th>
+                <th className="bold centered">POS</th>
+                <th className="bold centered">TEAM</th>
+                <th className="bold centered">GP</th>
+                <th className="bold centered">MPG</th>
+                <th className="bold centered">FG%</th>
+                <th className="bold centered">FGM</th>
+                <th className="bold centered">FGA</th>
+                <th className="bold centered">FT%</th>
+                <th className="bold centered">FTM</th>
+                <th className="bold centered">FTA</th>
+                <th className="bold centered">3PM</th>
+                <th className="bold centered">PTS</th>
+                <th className="bold centered">TREB</th>
+                <th className="bold centered">AST</th>
+                <th className="bold centered">STL</th>
+                <th className="bold centered">BLK</th>
+                <th className="bold centered">TO</th>
+                <th className="bold centered">TOTAL</th>
               </tr>
             </thead>
             <tbody>{playerRows}</tbody>
@@ -82,58 +82,58 @@ function AppUI({
       )}
       {selectedPlayer && (
         <div>
-          <p>Entered value:</p>
+          <p></p>
           <table className="bordered-table">
-            <thead>
+            <thead className="header-row">
               <tr>
-                <th className="centered">RANK</th>
-                <th className="centered">ADP</th>
-                <th className="centered">PLAYER</th>
-                <th className="centered">POS</th>
-                <th className="centered">TEAM</th>
-                <th className="centered">GP</th>
-                <th className="centered">MPG</th>
-                <th className="centered">FG%</th>
-                <th className="centered">FGM</th>
-                <th className="centered">FGA</th>
-                <th className="centered">FT%</th>
-                <th className="centered">FTM</th>
-                <th className="centered">FTA</th>
-                <th className="centered">3PM</th>
-                <th className="centered">PTS</th>
-                <th className="centered">TREB</th>
-                <th className="centered">AST</th>
-                <th className="centered">STL</th>
-                <th className="centered">BLK</th>
-                <th className="centered">TO</th>
-                <th className="centered">TOTAL</th>
+                <th className="bold centered">RANK</th>
+                <th className="bold centered">ADP</th>
+                <th className="bold centered">PLAYER</th>
+                <th className="bold centered">POS</th>
+                <th className="bold centered">TEAM</th>
+                <th className="bold centered">GP</th>
+                <th className="bold centered">MPG</th>
+                <th className="bold centered">FG%</th>
+                <th className="bold centered">FGM</th>
+                <th className="bold centered">FGA</th>
+                <th className="bold centered">FT%</th>
+                <th className="bold centered">FTM</th>
+                <th className="bold centered">FTA</th>
+                <th className="bold centered">3PM</th>
+                <th className="bold centered">PTS</th>
+                <th className="bold centered">TREB</th>
+                <th className="bold centered">AST</th>
+                <th className="bold centered">STL</th>
+                <th className="bold centered">BLK</th>
+                <th className="bold centered">TO</th>
+                <th className="bold centered">TOTAL</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="centered">{selectedPlayer.rank}</td>
-                <td className="centered">{selectedPlayer.adp}</td>
-                <td className="centered">{selectedPlayer.name}</td>
-                <td className="centered">{selectedPlayer.position}</td>
-                <td className="centered">{selectedPlayer.team}</td>
-                <td className="centered">{selectedPlayer.gp}</td>
-                <td className="centered">{selectedPlayer.minutesPerGame}</td>
-                <td className="centered">{selectedPlayer.fieldGoal}</td>
-                <td className="centered">{selectedPlayer.fieldGoalMade}</td>
-                <td className="centered">{selectedPlayer.fieldGoalAttempt}</td>
-                <td className="centered">{selectedPlayer.freeThrow}</td>
-                <td className="centered">{selectedPlayer.freeThrowMade}</td>
-                <td className="centered">{selectedPlayer.freeThrowAttempt}</td>
-                <td className="centered">{selectedPlayer.threePointMade}</td>
-                <td className={selectedPlayer.points >= 30 ? 'centered green' : 'centered'}>
+                <td className="bold centered">{selectedPlayer.rank}</td>
+                <td className="bold centered">{selectedPlayer.adp}</td>
+                <td className="bold centered">{selectedPlayer.name}</td>
+                <td className="bold centered">{selectedPlayer.position}</td>
+                <td className="bold centered">{selectedPlayer.team}</td>
+                <td className="bold centered">{selectedPlayer.gp}</td>
+                <td className="bold centered">{selectedPlayer.minutesPerGame}</td>
+                <td className="bold centered">{selectedPlayer.fieldGoal}</td>
+                <td className="bold centered">{selectedPlayer.fieldGoalMade}</td>
+                <td className="bold centered">{selectedPlayer.fieldGoalAttempt}</td>
+                <td className="bold centered">{selectedPlayer.freeThrow}</td>
+                <td className="bold centered">{selectedPlayer.freeThrowMade}</td>
+                <td className="bold centered">{selectedPlayer.freeThrowAttempt}</td>
+                <td className="bold centered">{selectedPlayer.threePointMade}</td>
+                <td className={selectedPlayer.points >= 30 ? 'bold centered green' : 'bold centered'}>
                   {selectedPlayer.points}
                 </td>
-                <td className="centered">{selectedPlayer.totalRebounds}</td>
-                <td className="centered">{selectedPlayer.assists}</td>
-                <td className="centered">{selectedPlayer.steals}</td>
-                <td className="centered">{selectedPlayer.blocks}</td>
-                <td className="centered">{selectedPlayer.turnovers}</td>
-                <td className="centered">{selectedPlayer.total}</td>
+                <td className="bold centered">{selectedPlayer.totalRebounds}</td>
+                <td className="bold centered">{selectedPlayer.assists}</td>
+                <td className="bold centered">{selectedPlayer.steals}</td>
+                <td className="bold centered">{selectedPlayer.blocks}</td>
+                <td className="bold centered">{selectedPlayer.turnovers}</td>
+                <td className="bold centered">{selectedPlayer.total}</td>
               </tr>
             </tbody>
           </table>
